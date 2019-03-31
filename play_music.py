63eed72a167e5a815658b20dc6ecda176da9593a
RoyYang0714 <sy2es93104@gmail.com>
2019-03-31 08:00:19 +0800

    .

---
+++ play_music.py
@@ -0,0 +1,40 @@
+import pygame
+import RPi.GPIO as GPIO
+
+LED_PIN=18
+LINKIT_IN_HOLD=22
+LINKIT_IN_STOP=23
+LINKIT_IN_MUS_1=6
+LINKIT_IN_MUS_2=12
+
+LINKIT_OUT_0=4
+LINKIT_OUT_1=14
+PWM_FREQ=200
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(LINKIT_IN_HOLD, GPIO.IN)
+GPIO.setup(LINKIT_IN_STOP, GPIO.IN)
+GPIO.setup(LINKIT_IN_MUS_1, GPIO.IN)
+GPIO.setup(LINKIT_IN_MUS_2, GPIO.IN)
+
+pygame.mixer.init()
+pygame.mixer.music.load("music.mp3")
+pygame.mixer.music.play()
+
+while pygame.mixer.music.get_busy() == True:
+	if GPIO.input(LINKIT_IN_HOLD)==0 or GPIO.input(LINKIT_IN_STOP)==1:
+		pygame.mixer.music.stop()
+		break
+
+	elif GPIO.input(LINKIT_IN_MUS_1)==0 and GPIO.input(LINKIT_IN_MUS_2)==0:
+		pygame.mixer.music.set_volume(0.1)
+
+	elif GPIO.input(LINKIT_IN_MUS_1)==0 and GPIO.input(LINKIT_IN_MUS_2)==1:
+		pygame.mixer.music.set_volume(0.3)
+
+	elif GPIO.input(LINKIT_IN_MUS_1)==1 and GPIO.input(LINKIT_IN_MUS_2)==0:
+		pygame.mixer.music.set_volume(0.6)
+
+	elif GPIO.input(LINKIT_IN_MUS_1)==1 or GPIO.input(LINKIT_IN_MUS_2)==1:
+		pygame.mixer.music.set_volume(1)
